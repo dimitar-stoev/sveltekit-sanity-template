6b3f1314b147941a72494d21035995cfd94efdce
Dimitar Stoev <dimitarg.stoev@gmail.com>
2024-04-01 20:59:32 +0300

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import './layout.module.scss';
+	import './navigation.module.scss';
+</script>
+
+<nav>
+	<h2>Stoictivity Club</h2>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/about">About</a>
+		</li>
+	</ul>
+</nav>
+
+<slot />
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '76097xg3',
+    dataset: 'production',
+  },
+})
